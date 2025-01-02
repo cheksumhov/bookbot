8c00813e8c0f7a2e612e02b8c3661e1f15afff8a
cheksumhov <matt@mattesmith.com>
2025-01-02 12:45:03 -0500

    Finished the program functionality and added more detailed comments

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 def main():
-    path = "books/frankenstein.txt"
-    text = get_text(path)
-    word_count = get_word_count(text)
-    print(word_count)
+    path = "books/frankenstein.txt" # Path to the book file
+    text = get_text(path) # Call the get_get() function, pass the path, store result in text variable
+    word_count = get_word_count(text) # Call the get_word_count() function, pass text, store result in word_count
+    char_count = get_char_count(text) # Call the char_count() function, pass text, stores result as a dictionary
 
-# Function to take the text and return count for each character
+# Print out a report of the text stats
+    print(f"## Generating report of {path}. ##")
+    print(f"{word_count} words found in the book.")
+    for x in char_count:
+        print(f"'{x}' was found {char_count[x]} times. ")
+    print("## Ending report. ##")
 
-# Take the text, split by whitespace, and return the length
+# Takes text and returns count for each character in the form of a dictionary
+def get_char_count(text):
+    chars_dict = {}
+    lower_text = text.lower()
+    for char in lower_text:
+        if char not in chars_dict:
+            chars_dict[char] = 1
+        else:
+            chars_dict[char] = (chars_dict[char] + 1)
+    return chars_dict
+
+# Takes the text, splits by whitespace, and returns the total word count
 def get_word_count(text):
     words = text.split()
     return len(words)
 
-# Take the path to the book, read the fiel, and return the text
+# Takes the path to the book, reads the file, returns the text in string format
 def get_text(path):
     with open(path) as f:
         return f.read()
