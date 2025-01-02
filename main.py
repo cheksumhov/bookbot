c4b048d386976695cdff3aa2062522eb6d0d96ee
cheksumhov <matt@mattesmith.com>
2025-01-02 11:47:49 -0500

    Corrections

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ def main():
     word_count = get_word_count(text)
     print(word_count)
 
+# Function to take the text and return count for each character
+
+# Take the text, split by whitespace, and return the length
 def get_word_count(text):
     words = text.split()
     return len(words)
 
+# Take the path to the book, read the fiel, and return the text
 def get_text(path):
     with open(path) as f:
         return f.read()
